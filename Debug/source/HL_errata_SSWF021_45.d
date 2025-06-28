296bd03ad79d86c7ce9b5ab8381425c87053f317
ram2424 <ramsekhar2424@gmail.com>
2025-06-28 17:43:21 +0530

    Adding the project blinking led for TMS570LC43

---
+++ Debug/source/HL_errata_SSWF021_45.d
@@ -0,0 +1,12 @@
+# FIXED
+
+source/HL_errata_SSWF021_45.obj: ../source/HL_errata_SSWF021_45.c
+source/HL_errata_SSWF021_45.obj: D:/Ram/TMS570LC43/Project_CCS/LedBlink/include/HL_errata_SSWF021_45_defs.h
+source/HL_errata_SSWF021_45.obj: D:/Ram/TMS570LC43/Project_CCS/LedBlink/include/HL_errata_SSWF021_45.h
+
+../source/HL_errata_SSWF021_45.c:
+
+D:/Ram/TMS570LC43/Project_CCS/LedBlink/include/HL_errata_SSWF021_45_defs.h:
+
+D:/Ram/TMS570LC43/Project_CCS/LedBlink/include/HL_errata_SSWF021_45.h:
+
